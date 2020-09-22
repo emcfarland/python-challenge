ac2406775bbb8896fbdd2d675f1f9023f32de8f8
Elliott McFarland <elliottmcf@gmail.com>
2020-09-21 23:02:24 -0400

    Removed second for loop, adjusted calculation of monthly change.

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,23 +12,38 @@ with open(csvpath) as csvFileStream:
     num_months = 0
     total = 0
     prev_profit = 0
-    change = []
+    change = 0
     tot = 0
+    gains = 0
+    losses = 0
 
     for row in csvreader:
+        
         num_months += 1
         curr_profit = int(row[1])
         total += curr_profit
+        
         if prev_profit != 0:
-            change.append(curr_profit - prev_profit)
+            change += (curr_profit - prev_profit)
+        
+        if (prev_profit < curr_profit) and ((curr_profit - prev_profit) > gains):
+            gains = curr_profit - prev_profit
+
+        elif (prev_profit > curr_profit) and ((curr_profit - prev_profit) < losses):
+            losses = curr_profit - prev_profit
+        
         prev_profit = curr_profit
-    for val in change:
-        tot += val
+    
+    # for val in change:
+    #     tot += val
+
 
     print(f'Financial Analysis')
     print(f'--------------------------------')
     print(f'Total Months: {num_months}')
     print('Total: '+'${:,}'.format(total))
-    print('${:,.2f}'.format(tot/(num_months-1)))
-    print('${:,}'.format(max(change)))
-    print('${:,}'.format(min(change)))
+    print('${:,.2f}'.format(change/(num_months-1)))
+    print('${:,}'.format(gains))
+    print('${:,}'.format(losses))
+    # print('${:,}'.format(max(change)))
+    # print('${:,}'.format(min(change)))
